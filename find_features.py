50ca9818d129dbff97f17fd90d37e7c530a5a0e7
Arun Debray <a.debray@gmail.com>
2013-11-15 14:31:36 -0800

    Modified python file for batch processing

--- find_features.py
+++ find_features.py
@@ -1,55 +1,72 @@
+#!/usr/bin/env python2.7
+
 from astropy.io import fits
 import math
 import sys
 import numpy
-filename = raw_input("Enter a fits file\n")
+
+# Modified this to accept a filename on the command line.
+# This will be easier to weave into larger scripts.
+#filename = raw_input("Enter a fits file\n")
+
+# Usage: ./findfeatures.py filename
+if len(sys.argv) == 1:
+	print 'Error: please provide a filename!'
+	exit()
+filename = sys.argv[1]
 hdulist = fits.open(filename)
 
 feature_list = []
 
 # find the mean
-print "Find the arithmetic mean"
+#print "Find the arithmetic mean"
 for x in range(0,20):
 	sumvalues = 0
 	for y in range(0,len(hdulist[1].data)):
 		if not math.isnan(hdulist[1].data[y][x]):
 			sumvalues += hdulist[1].data[y][x]
 	avg = sumvalues/len(hdulist[1].data)
-	print avg
+#	print avg
 	feature_list.append(avg)
 	
 # find the max and min
-print "Find the max"
-for x in range(0,20):
-	maxval = -sys.maxint-1
-	for y in range(0,len(hdulist[1].data)):
-		if not math.isnan(hdulist[1].data[y][x]):
-			if hdulist[1].data[y][x] > maxval:
-				maxval = hdulist[1].data[y][x]
-	print maxval
-	feature_list.append(maxval)
-print "Find the min"
-for x in range(0,20):
-	minval = sys.maxint
-	for y in range(0,len(hdulist[1].data)):
-		if not math.isnan(hdulist[1].data[y][x]):
-			if hdulist[1].data[y][x] < minval:
-				minval = hdulist[1].data[y][x]
-	print minval
-	feature_list.append(minval)
+#print "Find the max"
+#for x in range(0,20):
+#	filt_vals = [m[x] for m in hdulist[1].data if not math.isnan(m[x])]
+#	maxval = max([m[x] for m in hdulist[1].data if not math.isnan(m[x])])
+#	feature_list.append(max(filt_vals) if len(filt_vals) != 0 else 0) # I don't know how else to signal it
+#
+#	maxval = -sys.maxint-1
+#	for y in range(0,len(hdulist[1].data)):
+#		if not math.isnan(hdulist[1].data[y][x]):
+#			if hdulist[1].data[y][x] > maxval:
+#				maxval = hdulist[1].data[y][x]
+#	print maxval
+#	feature_list.append(maxval)
+#print "Find the min"
+#for x in range(0,20):
+#	minval = sys.maxint
+#	for y in range(0,len(hdulist[1].data)):
+#		if not math.isnan(hdulist[1].data[y][x]):
+#			if hdulist[1].data[y][x] < minval:
+#				minval = hdulist[1].data[y][x]
+#	print minval
+#	feature_list.append(minval)
 	
 # find the range
-print "Find the range"
-for x in range(0,20):
-	feature_list.append(feature_list[20+x] - feature_list[40+x])
+#print "Find the range"
+#for x in range(0,20):
+#	feature_list.append(feature_list[20+x] - feature_list[40+x])
 
 # find the standard deviation
-print "Find the standard deviation"
+#print "Find the standard deviation"
 for x in range(0,20):
 	sumsquares = 0
 	for y in range(0,len(hdulist[1].data)):
 		if not math.isnan(hdulist[1].data[y][x]):
-			sumsquare += (hdulist[1].data[y][x]-feature_list[x])**2
+			sumsquares += (hdulist[1].data[y][x]-feature_list[x])**2
 	variance = sumsquares/len(hdulist[1].data)
 	stdev = variance**0.5
 	feature_list.append(stdev)
+
+print ' '.join([str(n) for n in feature_list])
