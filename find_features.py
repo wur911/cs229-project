0990a36e9c5ef908bf46a5cf84dfc0df55cf8e1a
Lwu <ray@ubuntu.(none)>
2013-12-13 18:03:43 -0800

    Added the two plots to the writeup and added details to writeup

--- find_features.py
+++ find_features.py
@@ -27,46 +27,13 @@ for x in attributeIndices:
 			sumvalues += hdulist[1].data[y][x]
 	avg = sumvalues/len(hdulist[1].data)
 	featurelist.append(avg)
-print ",".join([str(n) for n in featurelist])
 
 # find the max and min
-#print "Find the max"
-#for x in range(0,20):
-#	filt_vals = [m[x] for m in hdulist[1].data if not math.isnan(m[x])]
-#	maxval = max([m[x] for m in hdulist[1].data if not math.isnan(m[x])])
-#	feature_list.append(max(filt_vals) if len(filt_vals) != 0 else 0) # I don't know how else to signal it
-#
-#	maxval = -sys.maxint-1
-#	for y in range(0,len(hdulist[1].data)):
-#		if not math.isnan(hdulist[1].data[y][x]):
-#			if hdulist[1].data[y][x] > maxval:
-#				maxval = hdulist[1].data[y][x]
-#	print maxval
-#	feature_list.append(maxval)
-#print "Find the min"
-#for x in range(0,20):
-#	minval = sys.maxint
-#	for y in range(0,len(hdulist[1].data)):
-#		if not math.isnan(hdulist[1].data[y][x]):
-#			if hdulist[1].data[y][x] < minval:
-#				minval = hdulist[1].data[y][x]
-#	print minval
-#	feature_list.append(minval)
-	
-# find the range
-#print "Find the range"
-#for x in range(0,20):
-#	feature_list.append(feature_list[20+x] - feature_list[40+x])
+for x in range(0,20):
+	filt_vals = [m[x] for m in hdulist[1].data if not math.isnan(m[x]) and m[x]!=0]
+	maxval = max(filt_vals)
+	minval = min(filt_vals)
+	feature_list.append(maxval if len(filt_vals) != 0 else 0)
+	feature_lit.append(minval if len(filt_Vals) != 0 else 0)
 
-# find the standard deviation
-#print "Find the standard deviation"
-#for x in range(0,20):
-#	sumsquares = 0
-#	for y in range(0,len(hdulist[1].data)):
-#		if not math.isnan(hdulist[1].data[y][x]):
-#			sumsquares += (hdulist[1].data[y][x]-feature_list[x])**2
-#	variance = sumsquares/len(hdulist[1].data)
-#	stdev = variance**0.5
-#	feature_list.append(stdev)
-
-#print ' '.join([str(n) for n in feature_list])
+print ",".join([str(n) for n in featurelist])
